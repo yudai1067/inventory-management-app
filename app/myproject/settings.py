18e40be211c939e18189b51dcee31a7353e19a1b
KURONECOPC\kuron <yudai1067@gmail.com>
2025-08-30 14:26:46 +0900

    ALLOWED_HOSTS修正

--- app/myproject/settings.py
+++ app/myproject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "false").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", [])
+ALLOWED_HOSTS = ["*"]
 
 LOGGING = {
     "version": 1,
